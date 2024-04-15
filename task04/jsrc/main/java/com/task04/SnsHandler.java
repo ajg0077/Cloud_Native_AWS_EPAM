1fafa60a215512dabbffeeb14eb614f7768b3430
Antony Joy George <antonyjoy.george@gmail.com>
2024-04-15 22:57:30 +0530

    commit 5

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,12 +2,9 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.syndicate.deployment.annotations.EventSource;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
-import com.syndicate.deployment.model.EventSourceType;
-import com.syndicate.deployment.model.RegionScope;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -17,7 +14,7 @@ import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-@DependsOn(resourceType = ResourceType.SNS_TOPIC, name = "lambda_topic")
+@SnsEventSource(targetTopic = "lambda_topic")
 @LambdaHandler(lambdaName = "sns_handler",
         roleName = "sns_handler-role",
         isPublishVersion = true,
