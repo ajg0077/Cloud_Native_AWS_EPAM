474a34a8d9208829c0bc18cd93c9848bf7614578
Antony Joy George <antonyjoy.george@gmail.com>
2024-04-15 20:47:24 +0530

    initial commit

---
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -0,0 +1,33 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@DependsOn(name = "lambda_topic",
+        resourceType = ResourceType.SNS_TOPIC)
+@LambdaHandler(lambdaName = "sns_handler",
+        roleName = "sns_handler-role",
+        isPublishVersion = true,
+        aliasName = "${lambdas_alias_name}",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class SnsHandler implements RequestHandler<SNSEvent, Void> {
+    @Override
+    public Void handleRequest(SNSEvent event, Context context) {
+        for (SNSEvent.SNSRecord record : event.getRecords()) {
+            String snsMessage = record.getSNS().getMessage();
+            context.getLogger().log("Received message from SNS: " + snsMessage);
+        }
+        return null;
+    }
+}
---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,32 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SqsTriggerEventSource(targetQueue = "async_queue",
+        batchSize = 10)
+@LambdaHandler(lambdaName = "sqs_handler",
+        roleName = "sqs_handler-role",
+        isPublishVersion = true,
+        timeout = 10,
+        aliasName = "${lambdas_alias_name}",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
+
+    @Override
+    public Void handleRequest(SQSEvent event, Context context) {
+        for (SQSEvent.SQSMessage message : event.getRecords()) {
+            String sqsMessageBody = message.getBody();
+            context.getLogger().log("Received message from SQS: " + sqsMessageBody);
+        }
+        return null;
+    }
+}
