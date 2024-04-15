995f801ace33f102b22b85c425b367beccb80eec
Antony Joy George <antonyjoy.george@gmail.com>
2024-04-15 21:48:05 +0530

    commit 4

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,9 +2,12 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.EventSource;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.EventSourceType;
+import com.syndicate.deployment.model.RegionScope;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -14,7 +17,6 @@ import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-@SnsEventSource(targetTopic = "lambda_topic")
 @DependsOn(resourceType = ResourceType.SNS_TOPIC, name = "lambda_topic")
 @LambdaHandler(lambdaName = "sns_handler",
         roleName = "sns_handler-role",
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -3,9 +3,11 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.EventSource;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.EventSourceType;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -14,7 +16,6 @@ import java.util.Map;
 
 @SqsTriggerEventSource(targetQueue = "async_queue",
         batchSize = 10)
-@DependsOn(resourceType = ResourceType.SQS_QUEUE, name = "async_queue")
 @LambdaHandler(lambdaName = "sqs_handler",
         roleName = "sqs_handler-role",
         isPublishVersion = true,
